c2ac9fc4d159e8e88e82729a2a08bb739659cb63
onkarsherkar <onkar.sherkar@gmail.com>
2025-12-10 22:25:35 +0530

    Result table added

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,38 @@
-import {calculateInvestmentResults} from "../util/investment"
+import {calculateInvestmentResults, formatter} from "../util/investment"
 
 export default function Results({userInput}){
     const resultData = calculateInvestmentResults(userInput);
+    const initialInvestment = 
+        resultData[0].valueEndOfYear -
+        resultData[0].interest -
+        resultData[0].annualInvestment;
     console.log("resultData", resultData);
     return(
-        <p>Results...</p>
+       <table id="result">
+        <thead>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+        </thead>
+        <tbody>
+            {resultData.map(yearData => {
+                const totalInterset = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - 
+                initialInvestment;
+                
+                const totalAmountInvested = yearData.valueEndOfYear - totalInterset;
+
+                return <tr key={yearData.year}>
+                    <td>{yearData.year}</td>
+                    <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                    <td>{formatter.format(yearData.interest)}</td>
+                    <td>{formatter.format(totalInterset)}</td>
+                    <td>{formatter.format(totalAmountInvested)}</td>
+                </tr>
+            })}
+        </tbody>
+       </table>
         
     )
 
